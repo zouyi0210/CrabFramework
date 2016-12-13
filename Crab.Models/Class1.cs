5da4eb816628f3758d34ce34a608a96d5f6ba39c
zouyi0210 <joey_zy0210@163.com>
2016-12-13 14:51:18 +0800

    创建螃蟹框架

---
+++ Crab.Models/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crab.Models
+{
+    public class Class1
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World.");
+            Console.ReadKey();
+        }
+    }
+}
